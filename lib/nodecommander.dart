4b3ab5f21ee55ab86c59a4d02659d18c7ad87989
synw <synw@yahoo.com>
2020-03-08 09:47:08 +0100

    Export getHost

--- lib/nodecommander.dart
+++ lib/nodecommander.dart
@@ -3,5 +3,6 @@ library nodecommander;
 export 'src/cli/cli.dart';
 export 'src/command/internal/commands.dart';
 export 'src/command/model.dart';
+export 'src/desktop/host.dart';
 export 'src/node.dart';
 export 'src/types.dart';
